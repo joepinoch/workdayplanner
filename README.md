6e554538e0b8b9085f8dd205c1cbb7913bd5c303
joepinoch <josephpineda@joey.local>
2023-03-27 23:27:02 -0700

    Module 5 updates

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # workdayplanner
-A planner for my work day :)
+
+A planner for my work day
+
+## I created this using the supplied code, a bit of tweaking the javascript as well! See screenshot below:
+
+![Screenshot of work](assets/images/screenshot1.jpeg)
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("currentDay").textContent = datetime;
+
+function run() {
+  var txt = document.getElementById("hour-9");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-10");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-11");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-12");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-1");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-2");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-3");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-4");
+  alert(txt);
+}
+
+function run() {
+  var txt = document.getElementById("hour-5");
+  alert(txt);
+}
+
+run();
